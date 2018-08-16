049da7c22b7c80626c6be985ee67255debf9884a
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-08-16 12:26:27 -0400

    Nunit Example Test case

---
+++ CustomerOrderService.Tests/CustomerOrderServiceTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using CustomerOrderService;
+
+
+namespace CustomerOrderService.Tests
+{
+    [TestFixture]
+    public class CustomerOrderServiceTests
+    {
+        [TestCase]
+        public void When_PremiumCustomer_Expect_10PercentDiscount()
+        {
+            Customer premiumCustomer = new Customer
+            {
+
+            };
+
+        }
+    }
+}
